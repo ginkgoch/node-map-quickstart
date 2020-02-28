9ea2be7174d6db12e5b27898cb92aa976990052e
Ginkgoch <ginkgoch@outlook.com>
2020-02-28 15:58:53 +0800

    Add web quick start client

--- quick-started-web/index.js
+++ quick-started-web/index.js
@@ -10,7 +10,7 @@ function serve() {
     const server = http.createServer(async (req, res) => {
         let handled = false;
         // handle the route handlers in a loop.
-        let routeHandlers = [handleTileRoute]; // new route handlers will be added in this array later
+        let routeHandlers = [handleTileRoute, handleIdentifyRoute]; // new route handlers will be added in this array later
         for (let routeHandler of routeHandlers) {
             handled = await routeHandler(req, res);
             if (handled) {
@@ -50,7 +50,7 @@ async function handleTileRoute(req, res) {
     let handled = false;
 
     // parse the route: /maps/default/{z}/{x}/{y}
-    if (req.url.match(/maps\/default(\/\d+){3}/)) {
+    if (req.url.match(/maps\/default(\/\d+){3}/ig)) {
         // parse x, y, z from url
         let segments = req.url.split('/');
         let [z, x, y] = segments.slice(segments.length - 3);
@@ -59,12 +59,67 @@ async function handleTileRoute(req, res) {
         let tileImage = await getTileImage(x, y, z);
 
         // write respond
-        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.writeHead(200, { 'Content-Type': 'image/png' });
         res.end(tileImage);
         handled = true;
     }
-    
+
     return handled;
+}
+
+// handle the identify route
+async function handleIdentifyRoute(req, res) {
+    let handled = false;
+
+    // parse the route: /maps/default/{layerName}/query
+    if (req.url.match(/maps\/default\/\w+\/query/ig)) {
+        let parameters = parseIdentifyParameters(req);
+        let features = await getIdentifyFeatures(parameters);
+        let featureCollection = new G.FeatureCollection(features);
+        res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+        res.end(JSON.stringify(featureCollection.toJSON()));
+        handled = true;
+    }
+
+    return handled;
+}
+
+async function getIdentifyFeatures(parameters) {
+    let projection = new G.Projection('WGS84', 'EPSG:3857');
+    let clickedPoint = new G.Point(parameters.lng, parameters.lat);
+    clickedPoint = projection.forward(clickedPoint);
+
+    let mapEngine = getMap();
+    let layer = mapEngine.layer('cntry02');
+    if (layer === undefined) {
+        return [];
+    }
+
+    await layer.open();
+    let features = await layer.source.query('intersection', clickedPoint);
+    features.forEach(f => f.geometry = projection.inverse(f.geometry));
+    return features;
+}
+
+function parseIdentifyParameters(req) {
+    let queryStringStarts = req.url.lastIndexOf('?');
+    let lat = undefined, lng = undefined, zoom = undefined;
+    if (queryStringStarts !== -1) {
+        let queryString = req.url.slice(queryStringStarts + 1);
+        queryString.split('&').map(s => s.split('=')).forEach(s => {
+            switch (s[0].toLowerCase()) {
+                case 'lat': lat = parseFloat(s[1]); break;
+                case 'lng': lng = parseFloat(s[1]); break;
+                case 'zoom': zoom = parseInt(s[1]); break;
+            }
+        });
+    }
+
+    let url = req.url.slice(0, queryStringStarts);
+    let segments = url.split('/');
+    let layer = segments[segments.length - 2];
+
+    return { layer, lat, lng, zoom };
 }
 
 serve();
